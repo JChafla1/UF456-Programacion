665ce5363cae6a03130f2a47588819a3e52e508e
JChafla1 <95426356+JChafla1@users.noreply.github.com>
2024-05-01 17:52:39 +0200

    Cercle

--- Interficies_abstractes/Cercle.java
+++ Interficies_abstractes/Cercle.java
@@ -1,5 +1,19 @@
 
+public class Cercle extends Figura2D {
+    private double radi;
 
-public class Cercle {
-    
+    public Cercle(String nom, double radi) {
+        super(nom);
+        this.radi = radi;
+    }
+
+    @Override
+    public double calcularArea() {
+        return Math.PI * radi * radi;
+    }
+
+    @Override
+    public double calcularPerimetre() {
+        return 2 * Math.PI * radi;
+    }
 }
