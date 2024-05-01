6773e5b3b190c5999c729345b05bb7924b6018f8
JChafla1 <95426356+JChafla1@users.noreply.github.com>
2024-05-01 17:54:11 +0200

    Main

--- Interficies_abstractes/Main.java
+++ Interficies_abstractes/Main.java
@@ -1,5 +1,15 @@
 
-
 public class Main {
-    
+    public static void main(String[] args) {
+        Cercle cercle = new Cercle("Cercle", 5.0);
+        Rectangle rectangle = new Rectangle("Rectangle", 4.0, 3.0);
+
+        System.out.println("Cercle:");
+        System.out.println("Àrea: " + cercle.calcularArea());
+        System.out.println("Perímetre: " + cercle.calcularPerimetre());
+
+        System.out.println("\nRectangle:");
+        System.out.println("Àrea: " + rectangle.calcularArea());
+        System.out.println("Perímetre: " + rectangle.calcularPerimetre());
+    }
 }
