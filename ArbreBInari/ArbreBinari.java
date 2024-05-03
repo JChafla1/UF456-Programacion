ecfcd6d0ebbf48dbcf4d43adcfb909deb7d7a85f
JChafla1 <95426356+JChafla1@users.noreply.github.com>
2024-05-03 12:09:43 +0200

    Update ArbreBinari.java

--- ArbreBInari/ArbreBinari.java
+++ ArbreBInari/ArbreBinari.java
@@ -1,4 +1,37 @@
 
+
 public class ArbreBinari {
+    private TreeNode root;
+    private int profundidad;
+    private static int instanceCount = 0; //Contador de instancias
+
+
+    // Sin especificar la profundidad
+    public ArbreBinari(){
+        instanceCount++;
+        root = null;
+        profundidad = 0;
+    }
+
+    // Especificando la profundidad
+    public ArbreBinari(int profundidad) {
+        instanceCount++;
+        root = null;
+        this.profundidad = profundidad;
+    }
+
+    // GETTERS Y SETTERS
+    public int getProfundidad(){
+        return profundidad;
+    }
+
+    public void setProfundidad(int profundidad) {
+        this.profundidad = profundidad;
+    }
+
+    public static int getInstanceCount(){
+        return instanceCount;
+    }
 
 }
+
