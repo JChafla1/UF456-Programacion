18534e72aef8404d11e87203e21407c23562046f
JChafla1 <95426356+JChafla1@users.noreply.github.com>
2024-05-03 12:16:19 +0200

    Add files via upload

---
+++ ArbreBInari/Main.java
@@ -0,0 +1,10 @@
+
+public class Main {
+    public static void main(String[] args) {
+        //Crear un arbol binario de enteros
+        ArbreBinari arbreEnters = new ArbreBinari();
+
+        // Crear un arbol binario de caracteres
+        ArbreBinari arbreCaracters = new arbreCaracters();
+    }
+}
