77c7f882860dca475fc10240272e5abedbb6251c
JChafla1 <95426356+JChafla1@users.noreply.github.com>
2024-05-07 13:14:04 +0200

    class_tarea

--- Gestor.java
+++ Gestor.java
@@ -3,3 +3,12 @@ import java.util.*;
 public class Gestor{
 
 }
+
+class Tarea {
+    private int id;
+    private String descripcio;
+    private Prioritat prioritat;
+    private boolean completada;
+
+
+}
