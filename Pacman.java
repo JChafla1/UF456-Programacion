c9bf2c74ba3080f8b16389aefd66f28bb984fff8
JChafla1 <95426356+JChafla1@users.noreply.github.com>
2024-05-15 18:33:11 +0200

    Contructor-pacman

--- Pacman.java
+++ Pacman.java
@@ -5,5 +5,14 @@ public class Pacman {
     private int posY;
     private int vidas;
     private int puntuacion;
-  
+
+    //Constructor de la classe Pacman.
+    public Pacman(int posX, int posY) {
+        this.posX = posX;
+        this.posY = posY;
+        this.vidas = 3; // Pacman comienza con 3 vidas
+        this.puntuacio = 0;
+    }
+
+    
 }
