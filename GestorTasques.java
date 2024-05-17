b34cbedecaa9b1d8805c0c464978d140cee3af61
JChafla1 <95426356+JChafla1@users.noreply.github.com>
2024-05-17 12:14:20 +0200

    correccion

--- GestorTasques.java
+++ GestorTasques.java
@@ -1,10 +1,10 @@
 import java.util.Scanner;
 
-public class GestorTasques {
+class GestorTasques {
  
 }
 
-class main {
+public class main {
  private static Scanner scanner = new Scanner(System.in);
 
  public static void main(String[] args) {
