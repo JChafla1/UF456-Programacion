34977440b15d6068f907aea4bbc98771829fd8ed
JChafla1 <95426356+JChafla1@users.noreply.github.com>
2024-05-17 12:04:21 +0200

    OPciones

--- GestorTasques.java
+++ GestorTasques.java
@@ -16,7 +16,32 @@ class main {
             System.out.println("4. Mostrar tareas");
             System.out.println("5. Mostrar tareas con prioridad");
             System.out.println("6. Salir");
-            
+
+         int opcion = scanner.nextInt();
+            scanner.nextLine(); 
+
+            switch (opcion) {
+                case 1:
+                    añadirTasca();
+                    break;
+                case 2:
+                    marcarCompletada();
+                    break;
+                case 3:
+                    borrarTasca();
+                    break;
+                case 4:
+                    mostrarTasques();
+                    break;
+                case 5:
+                    mostrarTasquesPrioritad();
+                    break;
+                case 6:
+                    System.exit(0);
+                    break;
+                default:
+                    System.out.println("Opcion invalida, añadi una opcion de las mostradas en la lista");
+            }
 
         }
     }
