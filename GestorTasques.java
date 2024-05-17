25450b7e836eadc6554481f5022d0ad96fdcfd2e
JChafla1 <95426356+JChafla1@users.noreply.github.com>
2024-05-17 11:59:19 +0200

    Opciones

--- GestorTasques.java
+++ GestorTasques.java
@@ -5,6 +5,20 @@ public class GestorTasques {
 }
 
 class main {
-    private static Scanner scanner = new Scanner(System.in);
+ private static Scanner scanner = new Scanner(System.in);
+
+ public static void main(String[] args) {
+        while (true) {
+            System.out.print("Que desear hacer?? ");
+            System.out.println("1.- Agregar nueva tarea");
+            System.out.println("2. Marcar tarea como completada");
+            System.out.println("3. Borrar tarea");
+            System.out.println("4. Mostrar tareas");
+            System.out.println("5. Mostrar tareas con prioridad");
+            System.out.println("6. Salir");
+            
+
+        }
+    }
     
 }
