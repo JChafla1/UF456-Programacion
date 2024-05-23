76fd4efe24e0a08cf620825876e6bea9110307cf
JChafla1 <95426356+JChafla1@users.noreply.github.com>
2024-05-23 13:26:45 +0200

    Estudiante

--- Estudiantes.java
+++ Estudiantes.java
@@ -1,3 +1,14 @@
+import java.util.Scanner;
+
+class Estudiante {
+    // Atributos
+    private String nombre;
+    private String apellido;
+    private int edad;
+    private ArrayList<String> materias;
+    
+}
+
 public class Estudiantes {
     
 }
