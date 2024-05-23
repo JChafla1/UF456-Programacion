81c637fa4c90122f5c5b9b7503bad2d410746765
JChafla1 <95426356+JChafla1@users.noreply.github.com>
2024-05-23 18:21:13 +0200

    IntroducirEliminar

--- GestionLibros.java
+++ GestionLibros.java
@@ -2,6 +2,18 @@
 // Clase generica LlistaLlibres
 class LlistaLlibres<T> {
     private List<T> lista;
+
+    public LlistaLlibres() {
+        lista = new ArrayList<>();
+    }
+
+    public void introducirLibro(T libro) {
+        lista.add(libro);
+    }
+
+    public void eliminarLibro(T libro) {
+        lista.remove(libro);
+    }
     
 }
 
