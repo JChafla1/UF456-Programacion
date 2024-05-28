ee69d6ebb2b7424477fe54eec57ae6928f3d2285
JChafla1 <95426356+JChafla1@users.noreply.github.com>
2024-05-28 15:52:15 +0200

    Cargador

--- JuegoDeRol/CargadorXML.java
+++ JuegoDeRol/CargadorXML.java
@@ -1,9 +1,21 @@
 package JuegoDeRol;
 
+import org.xml.sax.SAXException;
+
 public class CargadorXML {
 
     public static void cargarPersonajesDesdeXML(String archivo, Gestion gestion) {
 
+        try {
+            // Se carga y normaliza el archivo XML
+            File inputFile = new File(archivo);
+            DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+            Document doc = dBuilder.parse(inputFile);
+            doc.getDocumentElement().normalize();
+
+        }
+        
     }
     
 }
