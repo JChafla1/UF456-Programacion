e893b8d2d57bc4509cb8034ff1ea3268341c1f6f
JChafla1 <95426356+JChafla1@users.noreply.github.com>
2024-05-28 13:41:27 +0200

    Atributos

--- JuegoDeRol/Personaje.java
+++ JuegoDeRol/Personaje.java
@@ -1,5 +1,12 @@
 package JuegoDeRol;
 
 public class Personaje {
+    // Atributos
+    private String nombre;
+    private int nivel;
+    private int puntosDeVida;
+    private int puntosDeMana;
+    private String arma;
+    private String armadura;
     
 }
