2f81084ff33fb1c215312af8c04bf0514ab1e793
JChafla1 <95426356+JChafla1@users.noreply.github.com>
2024-05-28 15:56:05 +0200

    Nodes

--- JuegoDeRol/CargadorXML.java
+++ JuegoDeRol/CargadorXML.java
@@ -14,6 +14,23 @@ public class CargadorXML {
             Document doc = dBuilder.parse(inputFile);
             doc.getDocumentElement().normalize();
 
+            NodeList nList = doc.getElementsByTagName("personatge");
+
+            for (int temp = 0; temp < nList.getLength(); temp++) {
+                Node nNode = nList.item(temp);
+
+                // Se obtienen los valores de los elementos hijo de cada nodo <personatge>.
+                if (nNode.getNodeType() == Node.ELEMENT_NODE) {
+                    Element eElement = (Element) nNode;
+
+                    String nombre = eElement.getElementsByTagName("nom").item(0).getTextContent();
+                    int nivel = Integer.parseInt(eElement.getElementsByTagName("nivell").item(0).getTextContent());
+                    int puntosDeVida = Integer.parseInt(eElement.getElementsByTagName("puntsDeVida").item(0).getTextContent());
+                    int puntosDeMana = Integer.parseInt(eElement.getElementsByTagName("puntsDeMana").item(0).getTextContent());
+                    String arma = eElement.getElementsByTagName("arma").item(0).getTextContent();
+                    String armadura = eElement.getElementsByTagName("armadura").item(0).getTextContent();
+
+
         }
         
     }
