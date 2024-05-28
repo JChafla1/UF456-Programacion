11210996ff349764860ec4937969fae05e4edbc6
JChafla1 <95426356+JChafla1@users.noreply.github.com>
2024-05-28 16:19:14 +0200

    Instancia

--- JuegoDeRol/JuegoRol.java
+++ JuegoDeRol/JuegoRol.java
@@ -2,6 +2,9 @@ package JuegoDeRol;
 
 public class JuegoRol {
     public static void main(String[] args) {
+        
+        // Crear una instancia de Gestion
+        Gestion gestion = new Gestion();
 
     }
 }
