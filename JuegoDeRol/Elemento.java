21cc1e174949e62cf6d3d4e3ca93a14ef391b35f
JChafla1 <95426356+JChafla1@users.noreply.github.com>
2024-05-28 14:44:33 +0200

    Atributos

--- JuegoDeRol/Elemento.java
+++ JuegoDeRol/Elemento.java
@@ -1,5 +1,10 @@
 package JuegoDeRol;
 
 public class Elemento {
+    // Atributos
+    private String nombre;
+    private String arma;
+    private String armadura;
+    private String pociones;
     
 }
