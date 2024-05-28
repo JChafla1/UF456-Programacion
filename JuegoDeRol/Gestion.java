a193dc812e870a0d5eabf04e10ed5d5234c9c52d
JChafla1 <95426356+JChafla1@users.noreply.github.com>
2024-05-28 14:54:41 +0200

    Constructor

--- JuegoDeRol/Gestion.java
+++ JuegoDeRol/Gestion.java
@@ -6,5 +6,11 @@ public class Gestion {
     // Atributos
     private List<Personaje> personajes;
     private List<Elemento> elementos;
+
+    //Constructor
+    public Gestion() {
+        this.personajes = new ArrayList<>();
+        this.elementos = new ArrayList<>();
+    }
     
 }
