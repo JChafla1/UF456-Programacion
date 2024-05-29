1e72d965f35e29bc071992334942daeb779262e8
JChafla1 <95426356+JChafla1@users.noreply.github.com>
2024-05-29 23:06:18 +0200

    Electrodomesticos

--- Amazon/Electrodomestico.java
+++ Amazon/Electrodomestico.java
@@ -1,5 +1,18 @@
 package Amazon;
 
-public class Electrodomestico {
+public class Electrodomestico extends Producto {
+    // Atributos
+    private int mesesGarantia;
+
+    // Constructor
+    public Electrodomestico(int codigo, String nombre, double precio, int mesesGarantia) {
+        super(codigo, nombre, precio);
+        this.mesesGarantia = mesesGarantia;
+    }
+
+    // Permite cambiar los meses de garantia
+    public void establirGarantia(int mesesGarantia) {
+        this.mesesGarantia = mesesGarantia;
+    }
     
 }
