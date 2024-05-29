43f919a281881e87b7d22e35e7449ec17b10d0b0
JChafla1 <95426356+JChafla1@users.noreply.github.com>
2024-05-29 23:15:41 +0200

    Constructor

--- Amazon/Tienda.java
+++ Amazon/Tienda.java
@@ -1,5 +1,15 @@
 package Amazon;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Tienda {
-    
+    // Lista
+    private List<Producto> productos;
+
+    // Constructor
+    public Tienda() {
+        this.productos = new ArrayList<>();
+    }
+
 }
