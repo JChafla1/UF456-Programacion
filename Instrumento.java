3d060a8a9eac494effcf4dd41ae7ce232ab97156
JChafla1 <95426356+JChafla1@users.noreply.github.com>
2024-05-29 22:21:37 +0200

    ClaseAbstracta

--- Instrumento.java
+++ Instrumento.java
@@ -1,3 +1,21 @@
+
+// Clase abstracta, significa que no se pueden crear instancias directamente de ella
+//  Sólo puede ser heredada por otras clases.
+abstract class Instrument {
+    private String name;
+
+    public Instrument(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    // Método abstracto que deberá ser implementado por las subclases
+    public abstract void play();
+}
+
 public class Instrumento {
     
 }
