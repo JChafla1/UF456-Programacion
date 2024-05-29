5862c04e22f26c2350acd2001005588b32702ce2
JChafla1 <95426356+JChafla1@users.noreply.github.com>
2024-05-29 15:56:59 +0200

    LlibreTable

--- GestionBiblioteca/Biblioteca.sql
+++ GestionBiblioteca/Biblioteca.sql
@@ -4,3 +4,12 @@ CREATE TABLE Autor (
     nom VARCHAR(50),
     cognom VARCHAR(50)
 );
+
+CREATE TABLE Llibre (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    titol VARCHAR(100),
+    anyPublicacio INT,
+    autorId INT,
+    -- El "autorID" de esta tabla se coge del "id" de la tabla autor
+    FOREIGN KEY (autorId) REFERENCES Autor(id)
+);
