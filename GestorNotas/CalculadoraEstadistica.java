c93f3175749b3a814068e67241c2f6f8499ae661
JChafla1 <95426356+JChafla1@users.noreply.github.com>
2024-05-30 00:02:49 +0200

    CalcularMedia

--- GestorNotas/CalculadoraEstadistica.java
+++ GestorNotas/CalculadoraEstadistica.java
@@ -2,4 +2,12 @@ package GestorNotas;
 
 public class CalculadoraEstadistica {
     
+    // métodos estáticos para calcular diferentes estadísticas sobre un conjunto de notas
+    public static double calcularMedia(double[] notas) {
+        double suma = 0;
+        for (double nota : notas) {
+            suma += nota;
+        }
+        return suma / notas.length;
+    }
 }
