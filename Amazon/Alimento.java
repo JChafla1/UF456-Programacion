f6dc7f17eb26447087e5ce5fea6fcd6bfebf4d1d
JChafla1 <95426356+JChafla1@users.noreply.github.com>
2024-05-29 22:59:08 +0200

    ImportAtributo

--- Amazon/Alimento.java
+++ Amazon/Alimento.java
@@ -1,5 +1,18 @@
 package Amazon;
 
-public class Alimento {
-    
+import java.time.LocalDate;
+
+public class Alimento extends Producto{
+    // Atributos
+    private LocalDate fechaCaducidad;
+
+    // Constructor
+    public Alimento(int codigo, String nombre, double precio, LocalDate fechaCaducidad) {
+        super(codigo, nombre, precio);
+        this.fechaCaducidad = fechaCaducidad;
+    }
+
+    public void establecerFechaCaducidad(LocalDate fechaCaducidad) {
+        this.fechaCaducidad = fechaCaducidad;
+    }
 }
