19f0f0878a671d0e798b98cbc7981b6313f9cb6b
JChafla1 <95426356+JChafla1@users.noreply.github.com>
2024-05-29 17:42:33 +0200

    InsercionPeliculas

--- GestionCine/Cine.sql
+++ GestionCine/Cine.sql
@@ -17,3 +17,9 @@ CREATE TABLE Pelicula (
 -- Inserción de directores
 INSERT INTO Director (id, nom, cognom) VALUES (1, 'Steven', 'Spielberg');
 INSERT INTO Director (id, nom, cognom) VALUES (2, 'Christopher', 'Nolan');
+
+-- Inserción de películas
+INSERT INTO Pelicula (id, titol, anyEstrena, directorId) VALUES (1, 'Jurassic Park', 1993, 1);
+INSERT INTO Pelicula (id, titol, anyEstrena, directorId) VALUES (2, 'Inception', 2010, 2);
+INSERT INTO Pelicula (id, titol, anyEstrena, directorId) VALUES (3, 'Interstellar', 2014, 2);
+INSERT INTO Pelicula (id, titol, anyEstrena, directorId) VALUES (4, 'Schindler\'s List', 1993, 1);
