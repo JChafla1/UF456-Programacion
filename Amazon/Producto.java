a18a6737f646ee6a3a95afc76c723c032ef73e25
JChafla1 <95426356+JChafla1@users.noreply.github.com>
2024-05-29 22:53:17 +0200

    Override

--- Amazon/Producto.java
+++ Amazon/Producto.java
@@ -12,4 +12,13 @@ public abstract class Producto {
         this.nombre = nombre;
         this.precio = precio;
     }
+
+    // Las subclases de Producto tendrán que implementar este método para calcular el precio final
+    public abstract double calcularPrecioFinal();
+
+    // Devuelve una representación en forma de cadena de los atributos del producto
+    @Override
+    public String toString() {
+        return "Codigo: " + codigo + ", Nombre: " + nombre + ", Precio: " + precio;
+    }
 }
