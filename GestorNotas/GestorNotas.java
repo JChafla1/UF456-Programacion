d3d1043af813e000dae3ce9d8fec302cedcfd359
JChafla1 <95426356+JChafla1@users.noreply.github.com>
2024-05-29 23:56:51 +0200

    Import

--- GestorNotas/GestorNotas.java
+++ GestorNotas/GestorNotas.java
@@ -1,5 +1,14 @@
 package GestorNotas;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class GestorNotas {
-    
+    // Atributos
+    private Map<String, double[]> notasAlumnos;
+
+    // Constructor
+    public GestorNotas() {
+        notasAlumnos = new HashMap<>();
+    }
 }
