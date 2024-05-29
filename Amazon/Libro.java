4f9d4e08e36c67eb7160b0a48755296a47e6ec8f
JChafla1 <95426356+JChafla1@users.noreply.github.com>
2024-05-29 23:10:36 +0200

    Libro

--- Amazon/Libro.java
+++ Amazon/Libro.java
@@ -1,5 +1,25 @@
 package Amazon;
 
-public class Libro {
-    
+public class Libro extends Producto {
+    private String autor;
+    private int numPaginas;
+
+    public Libro(int codigo, String nombre, double precio, String autor, int numPaginas) {
+        super(codigo, nombre, precio);
+        this.autor = autor;
+        this.numPaginas = numPaginas;
+    }
+
+    @Override
+    public double calcularPrecioFinal() {
+        if (numPaginas > 300) {
+            return precio * 1.05; // 5% de recargo si el libro tiene más de 300 páginas
+        }
+        return precio;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + ", Autor: " + autor + ", Número de pàginas: " + numPagines;
+    }
 }
