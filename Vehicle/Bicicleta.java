b3c5696542ffc6f6cd67d6680785a7d9bfdc5498
JChafla1 <95426356+JChafla1@users.noreply.github.com>
2024-05-29 21:49:26 +0200

    Override

--- Vehicle/Bicicleta.java
+++ Vehicle/Bicicleta.java
@@ -3,12 +3,29 @@ package Vehicle;
 public class Bicicleta implements vehicle {
 
     // Atributos
-    private String tipus;
-    private int velocitatActual;
+    private String tipo;
+    private int velocidatActual;
 
     // Constructores
-    public Bicicleta(String tipus) {
-        this.tipus = tipus;
-        this.velocitatActual = 0;
+    public Bicicleta(String tipo) {
+        this.tipo = tipo;
+        this.velocidatActual = 0;
     }
+
+    // Lo mismo que en el archivo anterior
+    @Override
+    public void accelerar(int velocidat) {
+        this.velocidatActual += velocidat;
+    }
+
+    @Override
+    public void frenar() {
+        this.velocidatActual = 0;
+    }
+
+    @Override
+    public int obtenerVelocidadActual() {
+        return this.velocidatActual;
+    }
+
 }
