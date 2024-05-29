90f5c4478198689dd6d21a76cc9ba3c20d7cb7ce
JChafla1 <95426356+JChafla1@users.noreply.github.com>
2024-05-30 00:10:32 +0200

    RegistrarNotas

--- GestorNotas/Principal.java
+++ GestorNotas/Principal.java
@@ -4,5 +4,11 @@ public class Principal {
     public static void main(String[] args) {
         // Creación de una instancia de GestorNotes
         GestorNotas gestorNotas = new GestorNotas();
+
+        // Registra las notas de algunos alumnos
+        // Me he equivocado en la sintaxi de notas xD
+        gestorNotas.registrarNotss("Anna", new double[] {8.5, 9.0, 7.5});
+        gestorNotas.registrarNotss("Joan", new double[] {6.0, 7.5, 8.0});
+        gestorNotas.registrarNotss("Maria", new double[] {9.0, 8.5, 9.5});
     }
 }
