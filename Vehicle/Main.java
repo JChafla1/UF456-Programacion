378cfa3f3a14350e7b64408be8627fb392e77f80
JChafla1 <95426356+JChafla1@users.noreply.github.com>
2024-05-29 21:55:37 +0200

    InstanciaBicicleta

--- Vehicle/Main.java
+++ Vehicle/Main.java
@@ -10,6 +10,13 @@ public class Main {
         System.out.println("Velocidat actual del coche: " + cotxe.obtenerVelocidadActual() + " km/h");
         cotxe.frenar();
         System.out.println("Velocidat actual del coche despues de frenar: " + cotxe.obtenerVelocidadActual() + " km/h");
-    
+
+        // Crear una instància de Bicicleta
+        Bicicleta bici = new Bicicleta("Muntanya");
+        bici.accelerar(20);
+        System.out.println("Bicicleta tipo: " + bici.getTipo());
+        System.out.println("Velocidat actual de la bici: " + bici.obtenerVelocidadActual() + " km/h");
+        bici.frenar();
+        System.out.println("Velocidat actual de la bici despues de frenar: " + bici.obtenerVelocidadActual() + " km/h");
     }
 }
