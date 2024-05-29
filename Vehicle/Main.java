76989e46aa0b491831b52ce021779449a2e2eed2
JChafla1 <95426356+JChafla1@users.noreply.github.com>
2024-05-29 21:54:33 +0200

    InstanciaAutomovil

--- Vehicle/Main.java
+++ Vehicle/Main.java
@@ -2,6 +2,14 @@ package Vehicle;
 
 public class Main {
     public static void main(String[] args) {
+
+        // Crear una instància d'Automòbil
+        Automovil cotxe = new Automovil("Toyota");
+        cotxe.accelerar(60);
+        System.out.println("Automovil marca: " + cotxe.getMarca());
+        System.out.println("Velocidat actual del coche: " + cotxe.obtenerVelocidadActual() + " km/h");
+        cotxe.frenar();
+        System.out.println("Velocidat actual del coche despues de frenar: " + cotxe.obtenerVelocidadActual() + " km/h");
     
     }
 }
