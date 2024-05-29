4f0007e5b292b6ccf7965e486fde54770869d972
JChafla1 <95426356+JChafla1@users.noreply.github.com>
2024-05-29 11:59:56 +0200

    ClasseProfessor

--- Gestion.java
+++ Gestion.java
@@ -1,5 +1,12 @@
 // Inicio uml
 @startuml
-
+  
+// Clase profesor
+class Professor {
+    - nom: String
+    - departament: String
+    - cursos: List<Curs> // Se coge el curso de la siguiente clase
+}
+  
 @enduml
 // Final uml
