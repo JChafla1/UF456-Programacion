883e6c4f5870798e21ac46081ab91fbdbf0088c8
JChafla1 <95426356+JChafla1@users.noreply.github.com>
2024-05-30 00:00:49 +0200

    RegistrarObtener

--- GestorNotas/GestorNotas.java
+++ GestorNotas/GestorNotas.java
@@ -11,4 +11,14 @@ public class GestorNotas {
     public GestorNotas() {
         notasAlumnos = new HashMap<>();
     }
+
+    // Registrar Notas
+    public void registrarNotss(String nombreAlumno, double[] notas) {
+        notasAlumnos.put(nombreAlumno, notas);
+    }
+
+    // Obtener notas
+    public double[] obtenerNotas(String nombreAlumno) {
+        return notasAlumnos.get(nombreAlumno);
+    }
 }
