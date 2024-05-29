bc53bb2a08ae640adc3c292e40000300f4366592
JChafla1 <95426356+JChafla1@users.noreply.github.com>
2024-05-29 15:54:51 +0200

    Create

---
+++ GestionBiblioteca/Biblioteca.sql
@@ -0,0 +1,6 @@
+CREATE TABLE Autor (
+    -- el "id" sera incrementado cada vez que se use, para que no se repita
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    nom VARCHAR(50),
+    cognom VARCHAR(50)
+);
