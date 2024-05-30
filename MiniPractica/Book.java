96d13ab0b2a96404c1629adf69f9fd479fae7cdc
JChafla1 <95426356+JChafla1@users.noreply.github.com>
2024-05-30 22:21:48 +0200

    Constructores

--- MiniPractica/Book.java
+++ MiniPractica/Book.java
@@ -1,6 +1,28 @@
 package MiniPractica;
 
 public class Book {
+  // Atributos
+    private int id;
+    private String title;
+    private String author;
+    private int year;
 
+    // Constructor por defecto
+    public Book() {}
+
+    // útil para crear nuevos libros antes de ser insertados en la base de datos
+    public Book(String title, String author, int year) {
+        this.title = title;
+        this.author = author;
+        this.year = year;
+    }
+
+    // útil para crear objetos de libros con datos completos recuperados de la base de datos
+    public Book(int id, String title, String author, int year) {
+        this.id = id;
+        this.title = title;
+        this.author = author;
+        this.year = year;
+    }
 }
 
