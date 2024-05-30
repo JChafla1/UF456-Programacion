2c3ec61b206ab8e3462d126e9273785e60447e79
JChafla1 <95426356+JChafla1@users.noreply.github.com>
2024-05-30 17:35:14 +0200

    InsertarDirector

--- GestionCine/Conexion.java
+++ GestionCine/Conexion.java
@@ -5,5 +5,16 @@ public class Conexion {
     private static final String DB_URL = "jdbc:mysql://localhost:3306/cinema";
     private static final String USER = "root";
     private static final String PASS = "1234";
+
+    // Insertar Director
+    public static void insertDirector(Connection conn, int id, String nom, String cognom) throws SQLException {
+        String sql = "INSERT INTO Director (id, nom, cognom) VALUES (?, ?, ?)";
+        try (PreparedStatement pstmt = conn.prepareStatement(sql)) {
+            pstmt.setInt(1, id);
+            pstmt.setString(2, nom);
+            pstmt.setString(3, cognom);
+            pstmt.executeUpdate();
+        }
+    }
     
 }
