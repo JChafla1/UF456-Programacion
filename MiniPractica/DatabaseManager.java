13455e5481bf5d68ec950dbac4222743baf0143a
JChafla1 <95426356+JChafla1@users.noreply.github.com>
2024-05-30 22:37:07 +0200

    Update DatabaseManager.java

--- MiniPractica/DatabaseManager.java
+++ MiniPractica/DatabaseManager.java
@@ -1,5 +1,8 @@
 package MiniPractica;
 
 public class DatabaseManager {
+    private static final String URL = "jdbc:mysql://localhost:3306/LibraryDB";
+    private static final String USER = "yourUsername";
+    private static final String PASSWORD = "yourPassword";
     
 }
