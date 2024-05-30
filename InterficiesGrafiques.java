78ac639d4b475059f54a0228080a9bcaee895f56
JChafla1 <95426356+JChafla1@users.noreply.github.com>
2024-05-30 16:21:11 +0200

    CrearBoton

--- InterficiesGrafiques.java
+++ InterficiesGrafiques.java
@@ -5,7 +5,12 @@ public class InterficiesGrafiques extends Application{
     // Metodo start
     @Override
     public void start(Stage primaryStage) {
+        // Crear el boton
+        Button btn = new Button();
+        btn.setText("Clica aquí");
+        btn.setOnAction(event -> {
 
+        });
     }
 
     public static void main(String[] args) {
