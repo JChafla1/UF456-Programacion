14209e5a2ad566ee8662de848b163adb6a90c243
JChafla1 <95426356+JChafla1@users.noreply.github.com>
2024-05-30 17:32:17 +0200

    Atributos

--- GestionCine/Conexion.java
+++ GestionCine/Conexion.java
@@ -1,5 +1,9 @@
 package GestionCine;
 
 public class Conexion {
+    // Atributos
+    private static final String DB_URL = "jdbc:mysql://localhost:3306/cinema";
+    private static final String USER = "root";
+    private static final String PASS = "1234";
     
 }
