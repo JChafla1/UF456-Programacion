1a897cec948806ad345963d8b0e1b1fa97587790
JChafla1 <95426356+JChafla1@users.noreply.github.com>
2024-05-30 12:29:23 +0200

    JFrame

--- JPanel.java
+++ JPanel.java
@@ -1,6 +1,12 @@
+import javax.swing.JFrame;
 
 public class JPanel {
     public static void main(String[] args) {
         
+        // Crear el JFrame
+        JFrame frame = new JFrame("Ejemplo con botones de JPanel");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(400, 300);
+
     }
 }
