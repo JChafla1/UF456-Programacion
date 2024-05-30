d216947a1d804182148b15ee21c065b3805230fa
JChafla1 <95426356+JChafla1@users.noreply.github.com>
2024-05-30 22:42:09 +0200

    Imports

--- MiniPractica/DatabaseManager.java
+++ MiniPractica/DatabaseManager.java
@@ -1,8 +1,15 @@
 package MiniPractica;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class DatabaseManager {
     private static final String URL = "jdbc:mysql://localhost:3306/LibraryDB";
     private static final String USER = "yourUsername";
     private static final String PASSWORD = "yourPassword";
-    
+
+    public Connection connect() throws SQLException {
+        return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
 }
