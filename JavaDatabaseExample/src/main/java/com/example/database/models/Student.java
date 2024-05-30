be375907d8a6384cd5e89d855e6eb8dd4a4ea78d
JChafla1 <95426356+JChafla1@users.noreply.github.com>
2024-05-30 23:20:16 +0200

    Getters

--- JavaDatabaseExample/src/main/java/com/example/database/models/Student.java
+++ JavaDatabaseExample/src/main/java/com/example/database/models/Student.java
@@ -13,5 +13,29 @@ public class Student {
         this.lastName = lastName;
     }
 
+    // Getters
+    public int getId() {
+        return id;
+    }
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    // Setters
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
    
 }
