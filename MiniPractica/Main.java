83b92bbb209fc9f1554b958b8bb67d368701d3b9
JChafla1 <95426356+JChafla1@users.noreply.github.com>
2024-05-30 22:56:16 +0200

    RecueprarLibros

--- MiniPractica/Main.java
+++ MiniPractica/Main.java
@@ -8,5 +8,10 @@ public class Main {
         dbManager.addBook(new Book("El Quijote", "Miguel de Cervantes", 1605));
         dbManager.addBook(new Book("1984", "George Orwell", 1949));
 
+        // Recuperar y mostrar la lista de libros
+        for (Book book : dbManager.getBooks()) {
+            System.out.println(book);
+        }
+
     }
 }
