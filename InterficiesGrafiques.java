ce1c747472d5d4364e03f98ba08140c22d4d6943
JChafla1 <95426356+JChafla1@users.noreply.github.com>
2024-05-30 16:17:27 +0200

    MetodoStart

--- InterficiesGrafiques.java
+++ InterficiesGrafiques.java
@@ -2,6 +2,11 @@ import javafx.application.Application;
 
 public class InterficiesGrafiques extends Application{
 
+    // Metodo start
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
 
     public static void main(String[] args) {
         launch(args);
