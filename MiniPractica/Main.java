a330c1f2a503c01c092c873a10e5bb6b49bf2e78
JChafla1 <95426356+JChafla1@users.noreply.github.com>
2024-05-30 22:54:43 +0200

    AgregarLibros

--- MiniPractica/Main.java
+++ MiniPractica/Main.java
@@ -3,6 +3,10 @@ package MiniPractica;
 public class Main {
     public static void main(String[] args) {
         DatabaseManager dbManager = new DatabaseManager();
-        
+
+        // Agregar libros
+        dbManager.addBook(new Book("El Quijote", "Miguel de Cervantes", 1605));
+        dbManager.addBook(new Book("1984", "George Orwell", 1949));
+
     }
 }
