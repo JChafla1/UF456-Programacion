32a00d5df45fe1392c934c2873ae0af3f0d15d37
JChafla1 <95426356+JChafla1@users.noreply.github.com>
2024-05-30 12:32:39 +0200

    GridLayout

--- JPanel.java
+++ JPanel.java
@@ -1,12 +1,17 @@
+import java.awt.GridLayout;
 import javax.swing.JFrame;
 
 public class JPanel {
     public static void main(String[] args) {
-        
+
         // Crear el JFrame
         JFrame frame = new JFrame("Ejemplo con botones de JPanel");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(400, 300);
+
+        // Crear el JPanel amb un GridLayout
+        // 3 files, 3 columnes
+        JPanel panel = new JPanel(new GroupLayout(3, 3)); 
 
     }
 }
