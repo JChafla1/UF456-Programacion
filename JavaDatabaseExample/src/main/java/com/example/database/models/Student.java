ed9894c7fb7987e843ca0dec865b993df8526cbe
JChafla1 <95426356+JChafla1@users.noreply.github.com>
2024-05-30 23:19:21 +0200

    Atributos

---
+++ JavaDatabaseExample/src/main/java/com/example/database/models/Student.java
@@ -0,0 +1,17 @@
+package JavaDatabaseExample.src.main.java.com.example.database.models;
+
+public class Student {
+    // Atributos
+    private int id;
+    private String firstName;
+    private String lastName;
+
+    // Constructores
+    public Student(int id, String firstName, String lastName) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+   
+}
