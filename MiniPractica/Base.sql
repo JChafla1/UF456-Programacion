c1118e7524b18b109d4fbb4324c9e355bd990438
JChafla1 <95426356+JChafla1@users.noreply.github.com>
2024-05-30 22:09:38 +0200

    CreacionTabla

---
+++ MiniPractica/Base.sql
@@ -0,0 +1,9 @@
+CREATE DATABASE library;
+USE library;
+
+CREATE TABLE books (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    author VARCHAR(255) NOT NULL,
+    year INT NOT NULL
+);
