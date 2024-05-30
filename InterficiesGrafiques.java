4704836deccb45f28f656e9302617b0961d86e7f
JChafla1 <95426356+JChafla1@users.noreply.github.com>
2024-05-30 16:12:46 +0200

    Main

--- InterficiesGrafiques.java
+++ InterficiesGrafiques.java
@@ -1,5 +1,9 @@
 import javafx.application.Application;
 
 public class InterficiesGrafiques extends Application{
-    
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
