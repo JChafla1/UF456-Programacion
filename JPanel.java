4ef71fbf833b7cb8f977e679e1c55821ffb9ec95
JChafla1 <95426356+JChafla1@users.noreply.github.com>
2024-05-30 12:38:21 +0200

    ErrorSintaxis

--- JPanel.java
+++ JPanel.java
@@ -14,7 +14,7 @@ public class JPanel {
 
         // Crear el JPanel amb un GridLayout
         // 3 files, 3 columnes
-        JPanel panel = new JPanel(new GroupLayout(3, 3)); 
+        JPanel panel = new JPanel(new GridLayout(3, 3));
 
         // Crear i afegir botons al JPanel
         for (int i = 1; i <= 9; i++) {
